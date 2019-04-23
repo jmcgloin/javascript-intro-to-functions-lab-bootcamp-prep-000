4b1bc8be9a43e9b1789f38192183aa8dac716ce2
Jason McGloin <jason.mcgloin@gmail.com>
2019-04-23 02:22:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ let shout = (exclaim) => exclaim.toUpperCase();
 let whisper = (shh) => shh.toLowerCase();
 let logShout = (exclaim) => console.log(exclaim.toUpperCase());
 let logWhisper = (shh) => console.log(whisper(shh));
+let sayHiToGrandma = (hiGrammy) => {
+  if ( hiGrammy === "I love you, Grandma." ) {
+    return "I love you, too."
+  } else if ( hiGrammy.isLowerCase() ) {
+    return "I can't hear you!";
+  } else if ( hiGrammy.isUpperCase() ) {
+    return "YES INDEED!";
+  } 
+}

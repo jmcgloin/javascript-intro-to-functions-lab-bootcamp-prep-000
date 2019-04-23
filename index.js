8e39ce89f662ba14edb73de0fa5729916992f5a5
Jason McGloin <jason.mcgloin@gmail.com>
2019-04-23 02:17:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 let shout = (exclaim) => exclaim.toUpperCase();
 let whisper = (shh) => shh.toLowerCase();
+let logShout = (exclaim) => console.log(exclaim.toUpperCase());
+let logWhisper = (shh) => console.log(whisper(shh));
